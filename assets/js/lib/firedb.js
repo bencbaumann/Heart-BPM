4e291e36965ee42a3e829b3f061b7c2f7692c5ab
Ben Baumann <ben@thisisbam.com>
2017-11-01 07:07:10 -0400

    added basic project scaffold

---
+++ assets/js/lib/firedb.js
@@ -0,0 +1,13 @@
+var firebase = (function(){
+
+var config = {
+    apiKey: "AIzaSyArpVJaMCUo1GvPVUlSH7rxRIXtJPK-zYc",
+    authDomain: "heart-bpm.firebaseapp.com",
+    databaseURL: "https://heart-bpm.firebaseio.com",
+    projectId: "heart-bpm",
+    storageBucket: "heart-bpm.appspot.com",
+    messagingSenderId: "981427094326"
+  };
+firebase.initializeApp(config);
+
+})();
