24cbe8ae9f12e2b6b44c3c6a36a3e70a93d5a206
Ben Baumann <ben@thisisbam.com>
2017-11-04 18:10:13 -0400

    app.js > log out window location for debugging

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+/* this is for debugging and will be removed from the app */
+
+console.log(window.location.href);
+
+/* ****************************************************** */
+
+
 $('#submit').on('click', function(e){
     e.preventDefault();
     spotifyAuth();
