2a649034e5d1c0db9bdd04ddecf463f9b8e2545d
Doreen Wiggins <Wiggy77@aol.com>
2017-11-04 11:23:34 -0400

    heart calculator

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,75 @@
 $(document).ready(function () {
 
+
+    var userAge = 0;
+    var userWeight = 0;
+    var userGender = "";
+    var chosenActivity = "";
+    var maximumHeartrate = 0;
+    var targetHeartrate = 0;
+    var lowBeatsRange = 0;
+    var highBeatsRange = 0;
+
+
     // gathering input info when submit button is clicked
-    $("button").click(function () {
+    $("#form-id").submit("click", function () {
+        event.preventDefault();
+
+        userAge = $("#age").val().trim();
+        console.log(userAge);
+        userWeight = $("#weight").val().trim();
+        console.log(userWeight);
+        userGender = $("input[name='gender']:checked").val();
+        console.log(userGender);
 
         // calculator for target heart rate based on inputs from form
+        if (gender === "male") {
 
+            // maximum heart rate calculator with 4.5 added for men - 211.415, 0.5,  0.05 and 4.5 are constants
+            // formula developed by Dr. Dan Heil
+            maximumHeartrate = 211.415 - (0.5 * userAge) - (0.05 * userWeight) + 4.5;
 
-        var maximumHeartrate = 0;
-        var meditationHeartrate = 0;
-        var moderateHeartrate = 0;
-        var intenseHeartrate = 0;
-        var targetHeartrate = 0;
-        var userAge = 0;
-        var userWeight = 0;
-        var userGender = "";
-
-
-        if (gender === female) {
-
-            // maximum heart rate calculator for women - 211.415 and 0.05 are constants
+        } else {
+            // maximum heart rate calculator for women - 211.415, 0.5 and 0.05 are constants (we are including "prefer not to say" with this group)
+            // formula developed by Dr. Dan Heil
             maximumHeartrate = 211.415 - (0.5 * userAge) - (0.05 * userWeight);
 
         }
 
-        if (gender === male) {
-            // maximum heart rate calculator with 4.5 added for men - 211.415, 0.05 and 4.5 are constants
-            maximumHeartrate = 211.415 - (0.5 * userAge) - (0.05 * userWeight) + 4.5;
+        console.log(maximumHeartrate);
+
+
+        chosenActivity = ($('input[name=activity]:checked').val());
+        console.log(chosenActivity);
+
+        // if user chooses high intensity/cardio workout then target is 85% of maximum heart rate
+        if (chosenActivity === "HIIT/Cardio") {
+
+            targetHeartrate = maximumHeartrate * 85;
+
+            // if user chooses weightlifting/moderate workout then target is 50% of maximum heart rate
+        } else if (chosenActivity === "weights") {
+
+            targetHeartrate = maximumHeartrate * .50;
+
+            // if user chooses meditation/relaxation then target is slow heart rate to 
+        } else {
+            targetHeartrate = 50;
 
         }
-
-
-
-        // if user chooses cardio workout 85% of maximum heart rate
-        intenseHeartrate = maximumHeartrate * 85;
-
-        // access music with bpm range from Spotify
-
-        // if user chooses weightlifting/moderate workout 50% of maximum heart rate
-        moderateHeartrate = maximumHeartrate * .50;
-
-        // access music with bpm range from Spotify
-
-
-        // if user chooses meditation/relaxation
-        meditationHeartrate = 50;
-
-        // access music with bpm range from Spotify
-
-
-
-
-
     });
+
+
+    
+    console.log(targetHeartrate);
+
+
+    // we will use our targetHeartrate to determine tempo of songs range to search in Spotify +-10
+
+
+
+
+
+
 
 })
