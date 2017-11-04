e152758a5d641d1324c6f91d34a89a2d0f0ded9d
Ben Baumann <ben@thisisbam.com>
2017-11-04 17:10:20 -0400

    getting ready to pull the token out of the url

--- assets/js/lib/spotify.js
+++ assets/js/lib/spotify.js
@@ -42,9 +42,8 @@ function spotifyAuth(){
 
     var options = [];
     options.push('client_id=e42a332f9c9044748bcf6ae097bbe29d'); //Required. The client ID provided to you by Spotify when you register your application. 
-    options.push('redirect_uri=https://bencbaumann.github.io/Heart-BPM/spotifycallback.html');
+    options.push('redirect_uri=https://bencbaumann.github.io/Heart-BPM/spotifycallback.html');  // Required. The URI to redirect to after the user grants/denies permission. This URI needs to be entered in the URI whitelist that you specify when you register your application. 
     options.push('response_type=token'); // Required. Set it to “token”. 
-     // Required. The URI to redirect to after the user grants/denies permission. This URI needs to be entered in the URI whitelist that you specify when you register your application. 
     // options.state =''; // Optional, but strongly recommended. The state can be useful for correlating requests and responses. Because your redirect_uri can be guessed, using a state value can increase your assurance that an incoming connection is the result of an authentication request. If you generate a random string or encode the hash of some client state (e.g., a cookie) in this state variable, you can validate the response to additionally ensure that the request and response originated in the same browser. This provides protection against attacks such as cross-site request forgery. See RFC-6749.
     options.push('scope=user-read-private user-read-email'); // Optional. A space-separated list of scopes: see Using Scopes. 
     // options.show_dialog = ''; //Optional. Whether or not to force the user to approve the app again if they’ve already done so. If false (default), a user who has already approved the application may be automatically redirected to the URI specified by redirect_uri. If true, the user will not be automatically redirected and will have to approve the app again.
@@ -59,6 +58,11 @@ function spotifyAuth(){
     // window.location.href(redirect);
     // window.location = redirect;
 
+}
+
+console.log(window.location);
+
+function getToken(){
 
 }
 
