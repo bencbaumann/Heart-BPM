ef686344c4cee20cfca4dd9b97cbdcce6698f5a7
Ben Baumann <ben@thisisbam.com>
2017-11-05 22:46:48 -0500

    spotify

--- assets/js/lib/spotify.js
+++ assets/js/lib/spotify.js
@@ -167,7 +167,7 @@ function addTracksToPlaylist(user, playlist, tracks, callback){
         $.ajax({
             url: url,
             method: 'POST',
-            data: data,
+            data: JSON.stringify(data),
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
