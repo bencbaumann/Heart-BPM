587064b6f0bb9d6f9a29a378e82228d1cb505aaa
Ben Baumann <ben@thisisbam.com>
2017-11-06 19:44:43 -0500

    twitter

---
+++ assets/js/lib/twitter.js
@@ -0,0 +1,36 @@
+var twitter = {
+  apikey: "xl82tzCBkcTPLjYWt7Dmxdq7q",
+  secret: "3DpiW6nbTUqU1Rqt4YUjEoL8oEO7tN0d3lPOWm5BbgcL7yjBmp",
+  callback: "https://bencbaumann.github.io/Heart-BPM/twittercallback.html",
+  auth: "https://api.twitter.com/oauth/authorize",
+  authorize: callback => {
+    $.ajax({
+      url: twitter.auth,
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      },
+      success: function(reponse) {
+        callback(response);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  },
+  tweet: callback => {
+    $.ajax({
+      url: twitter.auth,
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      },
+      success: function(reponse) {
+        callback(response);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  }
+};
