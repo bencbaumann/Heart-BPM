ce3a2a45d989b3a96f583cc523ee2da0db99122c
Ben Baumann <ben@thisisbam.com>
2017-11-07 14:44:47 -0500

    twitter

--- assets/js/twitterapp.js
+++ assets/js/twitterapp.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('#twitterauth').on('click', function (e) {
         e.preventDefault();
+        console.log('twitterauth click');
         twitter.authorize();
     });
 });
