24662437c60ffea1103f29ca2b27ea510e9e3293
Ben Baumann <ben@thisisbam.com>
2017-11-09 13:49:55 -0500

    squashing bugs

--- assets/js/lib/uistate.js
+++ assets/js/lib/uistate.js
@@ -62,6 +62,9 @@ ui.showHome = () => {
 
 ui.show = view => {
     console.log(`Updating the view to ${view}`);
+    if(window.location.href.includes('spotifycallback')){
+        window.location = '/Heart-BPM';
+    }):
     window.sessionStorage.setItem('view', view);
         ui.states.filter(function(state){
             state === view ? $(`#${view}`).show() : $(`#${view}`).hide();
